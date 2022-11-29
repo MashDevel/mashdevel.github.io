45c9a2458c650d3b8ea667384176d9ad46778b77
Mike Ashcroft <MashDevel@users.noreply.github.com>
2022-11-29 18:21:19 -0500

    src

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+echo 'socketplay.com' > CNAME
+
+cd -
